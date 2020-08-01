8d829be35eed30b4ee72cc55933d3c81ad2668d0
NIKO09ES <nicolasesquivelruiz@gmail.com>
2020-08-01 15:03:24 -0500

    add Heroku

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const db = require('../../db/db.json');
 const { createNewNote } = require('../../lib/note');
+const { v4: uuidv4 } = require('uuid');
 
 router.get("/notes", (req, res) => {
     res.json(db);
@@ -9,7 +10,7 @@ router.get("/notes", (req, res) => {
 router.post("/notes", (req, res) => {
     // set id based on what the next index of the array will be
     // req.body.id = randomenumber;
-    // req.body.id = uuid.v4()
+    req.body.id = uuidv4()
     const note = createNewNote(req.body, db);
     res.json(note);
     
